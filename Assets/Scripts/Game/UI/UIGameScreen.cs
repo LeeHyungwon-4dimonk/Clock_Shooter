912534f3dc36380754e1fd6a6fa1f19564452a4f
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-12-16 23:19:32 +0900

    [Feat] MultiStackGauge UI
    중첩 게이지 UI 기능 구현

--- Assets/Scripts/Game/UI/UIGameScreen.cs
+++ Assets/Scripts/Game/UI/UIGameScreen.cs
@@ -5,14 +5,19 @@ public class UIGameScreen : UIBase
 {
     [SerializeField] private Button _settingButton;
 
-    [SerializeField] private Image _stackGauge;
+    [SerializeField] private Image[] _stackGaugeImgs;
+
+    [SerializeField] private Color[] _gaugeColors;
+    [SerializeField] private Color _emptyColor = Color.black;
 
     private float _stackCount = 0;
+    private float _dir;
 
     private void Awake()
     {
         _settingButton.onClick.AddListener(OnClickSetting);
-        InitializeStackUI();
+        _dir = Manager.Game.Direction;
+        StackUIInitialize();
     }
 
     private void OnEnable()
@@ -22,15 +27,24 @@ public class UIGameScreen : UIBase
 
     #region StackGauge
 
-    private void InitializeStackUI()
+    private void StackUIInitialize()
     {
-        _stackGauge.fillAmount = _stackCount;
+        for(int i = 0; i < _stackGaugeImgs.Length; i++)
+        {
+            _stackGaugeImgs[i].color = _emptyColor;
+            _stackGaugeImgs[i].fillAmount = 1;
+        }
     }
 
     private void StackUIUpdate(int prev, int cur)
     {
         _stackCount = Manager.Game.turnStack.Current;
-        _stackGauge.fillAmount = _stackCount / Manager.Game.Direction;
+
+        int colorIndex = Mathf.FloorToInt(_stackCount / _dir);
+
+        _stackGaugeImgs[0].color = 0 <= colorIndex? _gaugeColors[colorIndex % _gaugeColors.Length] : _emptyColor;
+        _stackGaugeImgs[1].color = 1 <= colorIndex ? _gaugeColors[(colorIndex - 1) % _gaugeColors.Length] : _emptyColor;
+        _stackGaugeImgs[0].fillAmount = ((float)_stackCount % _dir) / _dir;
     }
 
     #endregion
