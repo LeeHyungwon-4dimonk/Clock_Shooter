934e615b33c5ee90212cc1a0723370b963015992
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-12-17 00:00:27 +0900

    [Feat] MultiStackGauge UI - DOTween Apply
    중첩 게이지 UI에 DOTween 연출을 넣어 부드럽게 게이지가 증감하도록 설정

--- Assets/Scripts/Game/UI/UIGameScreen.cs
+++ Assets/Scripts/Game/UI/UIGameScreen.cs
@@ -1,17 +1,20 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIGameScreen : UIBase
 {
+    [Header("Buttons")]
     [SerializeField] private Button _settingButton;
 
+    [Header("Stack Gauge UI")]
     [SerializeField] private Image[] _stackGaugeImgs;
-
     [SerializeField] private Color[] _gaugeColors;
     [SerializeField] private Color _emptyColor = Color.black;
 
-    private float _stackCount = 0;
+    private float _curStack;
     private float _dir;
+    private Tween _stackTween;
 
     private void Awake()
     {
@@ -38,13 +41,40 @@ public class UIGameScreen : UIBase
 
     private void StackUIUpdate(int prev, int cur)
     {
-        _stackCount = Manager.Game.turnStack.Current;
+        float prevStack = _curStack;
+        _curStack = Manager.Game.turnStack.Current;
 
-        int colorIndex = Mathf.FloorToInt(_stackCount / _dir);
+        _stackTween?.Kill();
 
-        _stackGaugeImgs[0].color = 0 <= colorIndex? _gaugeColors[colorIndex % _gaugeColors.Length] : _emptyColor;
-        _stackGaugeImgs[1].color = 1 <= colorIndex ? _gaugeColors[(colorIndex - 1) % _gaugeColors.Length] : _emptyColor;
-        _stackGaugeImgs[0].fillAmount = ((float)_stackCount % _dir) / _dir;
+        if (prevStack == _curStack) return;
+
+        _stackTween = DOTween.To(() => prevStack,
+            x => 
+            { 
+                prevStack = x;
+                UpdateStackColor(prevStack);
+            },
+            _curStack,
+            0.3f
+            )
+            .SetEase(Ease.OutCubic);        
+    }
+
+    private void UpdateStackColor(float stackCount)
+    {
+        int colorIndex = Mathf.FloorToInt(stackCount / _dir);
+
+        _stackGaugeImgs[0].color =
+            0 <= colorIndex
+            ? _gaugeColors[colorIndex % _gaugeColors.Length]
+            : _emptyColor;
+
+        _stackGaugeImgs[1].color =
+            1 <= colorIndex
+            ? _gaugeColors[(colorIndex - 1) % _gaugeColors.Length]
+            : _emptyColor;
+
+        _stackGaugeImgs[0].fillAmount = (stackCount % _dir) / _dir;
     }
 
     #endregion
